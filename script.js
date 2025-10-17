48b5f74886d0591668fc38aea71f0f332768f255
nemes08 <semen.semen.uur@gmail.com>
2025-10-18 01:35:53 +0300

    Remove debug log and update event listener

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-670f7866c19f64e85390901351a4a4492d9990c2
-console.log("Pi Network bağış sistemi test amaçlı çalışıyor!");
 const donateBtn = document.getElementById("donateBtn");
 const status = document.getElementById("status");
 
